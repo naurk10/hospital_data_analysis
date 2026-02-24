ea60ca4de1e3c712199c9345e0ba4569230d3fe6
Kakos <gamerkakos@gmail.com>
2026-02-24 18:54:08 -0300

    Adicionando arquivo CSV de pacientes e atualizando o script

--- scripts/conexao_postgresql.py
+++ scripts/conexao_postgresql.py
@@ -2,8 +2,11 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 
-# Criar a pasta 'dados' se ela não existir
-os.makedirs('dados', exist_ok=True)
+# Caminho absoluto para a pasta 'dados'
+output_folder = r'C:\Users\super\Desktop\PROJETOS\hospital_data_analysis\dados'
+
+# Criar a pasta 'dados' se ela não existir (usando os.makedirs apenas para garantir)
+os.makedirs(output_folder, exist_ok=True)
 
 # String de conexão com PostgreSQL via SQLAlchemy
 engine = create_engine('postgresql+psycopg2://postgres:6463@localhost:5432/analise_hospitalar')
@@ -15,6 +18,6 @@ query = "SELECT * FROM pacientes;"
 df = pd.read_sql(query, engine)
 
 # Salvar os dados em um arquivo CSV na pasta 'dados'
-df.to_csv('dados/pacientes.csv', index=False)
+df.to_csv(os.path.join(output_folder, 'pacientes.csv'), index=False)
 
 print("Dados exportados para pacientes.csv com sucesso!")
